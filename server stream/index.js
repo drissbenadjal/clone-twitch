d9b401a34940bc949c40ec55f222202c7219d486
drissbenadjal <driss.benadjal@hotmail.com>
2023-02-28 23:41:43 +0100

    add base file

---
+++ server stream/index.js
@@ -0,0 +1,32 @@
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    mediaroot: './server/media',
+    webroot: './public',
+    allow_origin: '*'
+  },
+  trans: {
+    ffmpeg: '/usr/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
+  }
+};
+
+const nms = new NodeMediaServer(config);
+nms.run();
